68ce16da2381a0775fd2457ac21e141038b187da
Eltomoura <168350175+Eltomoura@users.noreply.github.com>
2024-05-01 13:50:42 +0000

    Update file.js

--- script/file.js
+++ script/file.js
@@ -1,26 +1,35 @@
 const fs = require('fs');
 
-module.exports.config = {
-	name: "file",
-	version: "2.4.3",
-	credits: "cliff",
-	cooldown: 0,
-	hasPrefix: false,
-	usage: "",
-	role: 2,
-};
+module.exports = {
+  config: {
+    name: "file",
+    version: "1.0",
+    author: "OtinXShiva",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Send bot script",
+    longDescription: "Send bot specified file ",
+    category: "owner",
+    guide: "{pn} file name. Ex: .{pn} filename"
+  },
 
-module.exports.run = async function ({ args, api, event }) {
-	const fileName = args[0];
-	if (!fileName) {
-		return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
-	}
+  onStart: async function ({ message, args, api, event }) {
+    const permission = ["61551989415558"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("𝑩𝒂𝒕𝒂𝒓𝒅 𝒊𝒏𝒅𝒊𝒈𝒏𝒆 𝒔𝒆𝒖𝒍 TOMOURA 🇨🇮 𝒑𝒆𝒖𝒕 𝒖𝒕𝒊𝒍𝒊𝒔𝒆𝒓 𝒄𝒆𝒕𝒕𝒆 𝒄𝒎𝒅 👿🖕🏽", event.threadID, event.messageID);
+    }
+    
+    const fileName = args[0];
+    if (!fileName) {
+      return api.sendMessage("𝚕𝚎 𝚏𝚒𝚌𝚑𝚒𝚎𝚛 ?? ಠωಠ.", event.threadID, event.messageID);
+    }
 
-	const filePath = __dirname + `/${fileName}.js`;
-	if (!fs.existsSync(filePath)) {
-		return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
-	}
+    const filePath = __dirname + `/${fileName}.js`;
+    if (!fs.existsSync(filePath)) {
+      return api.sendMessage(`𝐃𝐞𝐬𝐨𝐥𝐞́ 𝐛𝐨𝐬𝐬 𝐜𝐞𝐭𝐭𝐞 𝐜𝐦𝐝 𝐧'𝐞𝐱𝐢𝐬𝐭𝐞 𝐩𝐚𝐬 𝐝𝐚𝐧𝐬 𝐦𝐞𝐬 𝐜𝐦𝐝𝐬 [✖]: ${fileName}.js`, event.threadID, event.messageID);
+    }
 
-	const fileContent = fs.readFileSync(filePath, 'utf8');
-	api.sendMessage({ body: fileContent }, event.threadID);
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    api.sendMessage({ body: fileContent }, event.threadID);
+  }
 };
