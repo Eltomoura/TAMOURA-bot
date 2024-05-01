18188426dbce3c792ea44567eff4dc7e9a21ec7a
Eltomoura <168350175+Eltomoura@users.noreply.github.com>
2024-05-01 13:42:28 +0000

    Update Ai.js

--- script/Ai.js
+++ script/Ai.js
@@ -1,29 +1,56 @@
 const axios = require('axios');
 
-module.exports.config = {
-		name: "Ai",
-		version: 1.0,
-		credits: "OtinXSandip",
-		description: "AI",
-		hasPrefix: false,
-		usages: "{pn} [prompt]",
-		aliases: ["megan","AE"],
-		cooldown: 0,
-};
+const Prefixes = [
+  '/ai',
+  'bot',
+  'salut',
+  '+ai',
+  'hum',
+  'ai',
+  'ask',
+];
 
-module.exports.run = async function ({ api, event, args }) {
-		try {
-				const prompt = args.join(" ");
-				if (!prompt) {
-						await api.sendMessage("Hey I'm your virtual assistant, ask me a question.", event.threadID);
-						return;
-				}
+module.exports = {
+  config: {
+    name: "ask",
+    version: 1.0,
+    author: "OtinXSandip",
+    longDescription: "AI",
+    category: "ai",
+    guide: {
+      en: "{p} questions",
+    },
+  },
+  onStart: async function () {},
+  onChat: async function ({ api, event, args, message }) {
+    try {
+      
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!prefix) {
+        return; // Invalid prefix, ignore the command
+      }
+      const prompt = event.body.substring(prefix.length).trim();
+   if (!prompt) {
+        await message.reply("⚪TOMOURA く悔 🟢 \n.....................\n 𝒔𝒂𝒍𝒖𝒕 𝒂𝒍𝒍𝒆𝒛-𝒚 𝒑𝒐𝒔𝒆́ 𝒗𝒐𝒕𝒓𝒆 𝒒𝒖𝒆𝒔𝒕𝒊𝒐𝒏 🇨🇮  ");
+        return;
+      }
 
-				const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
-				const answer = response.data.answer;
 
-				await api.sendMessage(answer, event.threadID);
-		} catch (error) {
-				console.error("Error:", error.message);
-		}
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
+
+ 
+    await message.reply({ body: `🟢TOMOURA く悔 ⚪
+ 
+     ━━━━━━ - ━━━━━━
+
+${answer}
+
+━━━━━━ - ━━━━━━ `,
+});
+
+   } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
 };
